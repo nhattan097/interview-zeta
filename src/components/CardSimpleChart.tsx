d361ba1745adb56c31c325c72b656a88e0deced4
Nhat Tan <tan@e9.digital>
2022-04-21 08:35:44 +0700

    Feature: card simple chart

---
+++ src/components/CardSimpleChart.tsx
@@ -0,0 +1,73 @@
+import { Flex, Text } from "@chakra-ui/react";
+import {
+  ResponsiveContainer,
+  BarChart,
+  Bar,
+  CartesianGrid,
+  Tooltip,
+} from "recharts";
+
+const data = [
+  {
+    name: "Page A",
+    uv: 4000,
+    pv: 2400,
+    amt: 2400,
+  },
+  {
+    name: "Page B",
+    uv: 3000,
+    pv: 1398,
+    amt: 2210,
+  },
+  {
+    name: "Page C",
+    uv: 2000,
+    pv: 9800,
+    amt: 2290,
+  },
+  {
+    name: "Page D",
+    uv: 2780,
+    pv: 3908,
+    amt: 2000,
+  },
+  {
+    name: "Page E",
+    uv: 1890,
+    pv: 4800,
+    amt: 2181,
+  },
+  {
+    name: "Page F",
+    uv: 2390,
+    pv: 3800,
+    amt: 2500,
+  },
+  {
+    name: "Page G",
+    uv: 3490,
+    pv: 4300,
+    amt: 2100,
+  },
+];
+
+const CardSimpleChart = () => {
+  return (
+    <Flex gap={10} flexDirection="column" flex={1} h="full">
+      <Text fontSize="md" fontWeight="semibold" textAlign="left">
+        Chart Title
+      </Text>
+      <ResponsiveContainer width="99%" height="100%">
+        <BarChart data={data}>
+          <CartesianGrid strokeDasharray="3 3" />
+          <Tooltip />
+          <Bar dataKey="pv" fill="#8884d8" />
+          <Bar dataKey="uv" fill="#82ca9d" />
+        </BarChart>
+      </ResponsiveContainer>
+    </Flex>
+  );
+};
+
+export default CardSimpleChart;
