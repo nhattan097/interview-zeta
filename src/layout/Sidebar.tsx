63a2f59f985bf053986152ed3fd4d97fc00077b9
Nhat Tan <tan@e9.digital>
2022-04-21 08:51:52 +0700

    Update: side bar

--- src/layout/Sidebar.tsx
+++ src/layout/Sidebar.tsx
@@ -6,6 +6,7 @@ import {
   StackDivider,
   Text,
 } from "@chakra-ui/react";
+import React from "react";
 import NavLink from "../components/NavLink";
 import { Logo } from "../Logo";
 import { _nav } from "../_nav";
@@ -22,8 +23,8 @@ const Sidebar = () => {
             return <NavLink key={nav.url} {...nav} />;
           }
           return (
-            <>
-              <Box w="full" px={4} py={5} pb={0} key={index}>
+            <React.Fragment key={index}>
+              <Box w="full" px={4} py={5} pb={0}>
                 <Text textAlign="left" textColor="#a4a4a4">
                   {nav.name}
                 </Text>
@@ -31,7 +32,7 @@ const Sidebar = () => {
                   nav.child.map((item) => <NavLink key={item.url} {...item} />)}
               </Box>
               <StackDivider borderWidth="1px" borderColor="gray.200" />
-            </>
+            </React.Fragment>
           );
         })}
       </VStack>
